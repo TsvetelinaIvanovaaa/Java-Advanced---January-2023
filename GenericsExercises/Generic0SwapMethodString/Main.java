3ccd56c5df6d9b0601b8cba8ab6f19b59125fbaf
Tsvetelina Ivanova <Tsvetelina.Ivanovaaa@gmail.com>
2023-04-29 13:37:11 +0300

    Problems solutions for Generics Exercises
    Add tasks for Generics Exercises

---
+++ GenericsExercises/Generic0SwapMethodString/Main.java
@@ -0,0 +1,25 @@
+package Generics.Exercises.Generic0SwapMethodString;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        GenericList<String> list = new GenericList<>();
+
+        int n = Integer.parseInt(reader.readLine());
+
+        for (int cycles = 0; cycles < n; cycles++) {
+            list.add(reader.readLine());
+        }
+
+        int[] swap = Arrays.stream(reader.readLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        list.swap(swap[0], swap[1]);
+
+        list.printToString();
+    }
+}
